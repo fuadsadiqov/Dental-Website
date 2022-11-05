2aebda75eb2d4a2f73c1975a0c00fcf0aaba8bbc
Fuad303 <sadiqovfuad8@gmail.com>
2022-11-05 11:45:34 +0400

    some possibly meaningful message

--- script.js
+++ script.js
@@ -29,3 +29,40 @@ function mobileCloseFunction(){
     mobileSearch.style.display = "none";
     mobileClose.style.display = "none";
 }
+ // Top Function  
+ let mybutton = document.getElementById("myBtn");
+ window.onscroll = function() {scrollFunction()};
+ 
+ function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     mybutton.style.display = "block";
+   } else {
+     mybutton.style.display = "none";
+   }
+ }
+ function topFunction() {
+   document.body.scrollTop = 0;
+   document.documentElement.scrollTop = 0;
+ }
+ // End of Top Function
+
+// Accordion
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for(i = 0; i< acc.length; i++){
+  acc[i].addEventListener("click", function(){
+    this.classList.toggle = "active";
+    let panel = this.nextElementSibling;
+    if(panel.style.display === "block"){
+      panel.style.display = "none";
+    }
+      else{
+        panel.style.display = "block";
+    }
+    
+  });
+}
+$(".service-item").click(function () {
+  $(this).children(".accordion").children("i").toggleClass("down");
+});
